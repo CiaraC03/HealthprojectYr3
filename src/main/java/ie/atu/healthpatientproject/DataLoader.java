41edfdd189eab18cd163ef977510d4ed36478ab0
ciara <g00402357@atu.ie>
2023-12-28 10:33:06 +0000

    added annotations and added saved data to dataloader

--- src/main/java/ie/atu/healthpatientproject/DataLoader.java
+++ src/main/java/ie/atu/healthpatientproject/DataLoader.java
@@ -2,7 +2,9 @@ package ie.atu.healthpatientproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements CommandLineRunner {
     private final PatientRepository patientRepository;
 
@@ -19,5 +21,8 @@ public class DataLoader implements CommandLineRunner {
         Patient testData = new Patient(1L, "Amy", "McCarthy", "Female", 24, "0851134567", "Amy@atu.ie", "Mervue, Galway");
         Patient testData2 = new Patient(2L, "Brian", "Sexton", "Male", 28, "0851638767", "Brian@atu.ie", "Ennis, Clare");
         Patient testData3 = new Patient(3L, "Cathy", "Cogan", "Female", 19, "0874653891", "Cathy@atu.ie", "Westport, Mayo");
+        patientRepository.save(testData);
+        patientRepository.save(testData2);
+        patientRepository.save(testData3);
     }
 }
